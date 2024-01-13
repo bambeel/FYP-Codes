d962a900fbe5e9f36b7002ed7357602936e1eafa
BamBeels <126537477+bambeel@users.noreply.github.com>
2024-01-13 15:42:03 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+const ListOfPanels=[
+    {
+        panelnum: '1',
+        voltage: '22',
+        amperes: '4',
+        irradiance: '250',
+        surfacetemp: '36',
+        power: '10',
+        cond: 'good',
+        comms: 'online',
+    },
+    {
+        panelnum: '2',
+        voltage: '23',
+        amperes: '6',
+        irradiance: '20',
+        surfacetemp: '06',
+        power: '10',
+        cond: 'defect',
+        comms: 'online',
+    },
+    {
+        panelnum: '0',
+        voltage: '0',
+        amperes: '0',
+        irradiance: '0',
+        surfacetemp: '0',
+        power: '0',
+        cond: '0',
+        comms: 'offline',
+    }   
+]
